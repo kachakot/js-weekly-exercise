8c8e9687b58710f118bd75b3055a1724bd27a819
Katarzyna <Kotynska>
2016-08-11 09:23:33 +0200

    Added .eslintrc.js

--- js-excercise-01/js/main.js
+++ js-excercise-01/js/main.js
@@ -1,27 +1,27 @@
 $(document).ready(function() {
-    var $menuItems = $('.js-trigger');
-    var $item = $menuItems.find('li a');
-    var $category = $('.js-content').find('li');
+	var $menuItems = $(".js-trigger");
+	var $item = $menuItems.find("li a");
+	var $category = $(".js-content").find("li");
 
-    $item.on('click', function(e) {
-        e.preventDefault();
-        $menuItems.each(function(index) {
-            $item.removeClass('active');
-        });
-        $clickedItem = $(this);
-        $clickedItem.toggleClass('active');
-        var currentCategory = $clickedItem.text().replace(' ', '-');
-        $category.each(function(index) {
-            var $categoryItem = $(this);
-            if ($categoryItem.text().indexOf(currentCategory) <= 0) {
-                $categoryItem.addClass('hidden');
-            }
-            else {
-                $categoryItem.removeClass('hidden');
-            }
-        });
-        if (currentCategory == 'All') {
-            $category.removeClass('hidden');
-        }
-    })
-})
+	$item.on("click", function(e) {
+		e.preventDefault();
+		$menuItems.each(function() {
+			$item.removeClass("active");
+		});
+		var clickedItem = $(this);
+		clickedItem.toggleClass("active");
+		var currentCategory = clickedItem.text().replace(" ", "-");
+		$category.each(function() {
+			var categoryItem = $(this);
+			if (categoryItem.text().indexOf(currentCategory) <= 0) {
+				categoryItem.addClass("hidden");
+			}
+			else {
+				categoryItem.removeClass("hidden");
+			}
+		});
+		if (currentCategory == "All") {
+			$category.removeClass("hidden");
+		}
+	});
+});
