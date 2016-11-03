99d1f46043fea77c643fe82cfe3038f72055b528
Kasia K <k.kotynska91@gmail.com>
2016-11-03 20:41:59 +0100

    Refactor code

--- js-exercise-01-v2/js/main.js
+++ js-exercise-01-v2/js/main.js
@@ -1,19 +1,17 @@
-var menuItems = document.querySelector(".js-trigger");
-var item = menuItems.querySelector("a");
-var items = menuItems.querySelectorAll("a"); //tablica nazw kategorii
-var content = document.querySelector(".js-content");
-var category = content.querySelectorAll("li"); //tablica kategorii
+var menuItems = document.querySelector(".js-trigger"),
+    items = menuItems.querySelectorAll("a"), // names of categories array
+    content = document.querySelector(".js-content"),
+    category = content.querySelectorAll("li"); // categories array
 
-for (var i = 0; i < items.length; i++){
-    items[i].addEventListener('click', function(e){
+for (var i = 0; i < items.length; i++) {
+    items[i].addEventListener('click', function(e) {
+        e.preventDefault();
         for(var j = 0; j < items.length; j++ ) {
             items[j].classList.remove("active");
         }
         var clickedItem = this;
-        e.preventDefault();
         clickedItem.classList.add('active');
         var currentCategory = clickedItem.innerHTML.replace(' ','-');
-        console.log(currentCategory);
         for(var k = 0; k < category.length; k++) {
             if(category[k].innerHTML.indexOf(currentCategory) <=0 ) {
                 category[k].classList.add('hidden');
