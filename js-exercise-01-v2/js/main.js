75a5f4e3033028166ec6b87750e0ce1ae3d9ed5b
Katarzyna Kotynska <katarzyna.kotynska@pagepro.co>
2016-11-04 12:36:01 +0100

    Refactor code - added eslint

--- js-exercise-01-v2/js/main.js
+++ js-exercise-01-v2/js/main.js
@@ -1,27 +1,31 @@
-var menuItems = document.querySelector(".js-trigger"),
-    items = menuItems.querySelectorAll("a"), // names of categories array
-    content = document.querySelector(".js-content"),
-    category = content.querySelectorAll("li"); // categories array
+/* global document */
+var menuItems = document.querySelector('.js-trigger'),
+    items = menuItems.querySelectorAll('a'), // names of categories array
+    content = document.querySelector('.js-content'),
+    category = content.querySelectorAll('li'), // categories array
+    currentCategory,
+    i,
+    j,
+    k,
+    l;
 
-for (var i = 0; i < items.length; i++) {
-    items[i].addEventListener('click', function(e) {
+for (i = 0; i < items.length; i++) {
+    items[i].addEventListener('click', function (e) {
         e.preventDefault();
-        for(var j = 0; j < items.length; j++ ) {
-            items[j].classList.remove("active");
+        for  (j = 0; j < items.length; j++ ) {
+            items[j].classList.remove('active');
         }
         var clickedItem = this;
         clickedItem.classList.add('active');
-        var currentCategory = clickedItem.innerHTML.replace(' ','-');
-        for(var k = 0; k < category.length; k++) {
-            if(category[k].innerHTML.indexOf(currentCategory) <=0 ) {
+        currentCategory = clickedItem.innerHTML.replace(' ', '-');
+        for (k = 0; k < category.length; k++) {
+            if (category[k].innerHTML.indexOf(currentCategory) <= 0 ) {
                 category[k].classList.add('hidden');
             }
-            else {
-                category[k].classList.remove('hidden');
-            }
+            category[k].classList.remove('hidden');
         }
-        for(var l = 0; l < category.length; l++) {
-            if (currentCategory == 'All') {
+        for (l = 0; l < category.length; l++) {
+            if (currentCategory === 'All') {
                 category[l].classList.remove('hidden');
             }
         }
--- js-exercise-01/js/main.js
+++ js-exercise-01/js/main.js
@@ -1,27 +1,27 @@
-$(document).ready(function() {
-	var $menuItems = $(".js-trigger"),
-		$item = $menuItems.find("li a"),
-		$category = $(".js-content").find("li");
+/* global document, $ */
+$(document).ready(function () {
+    var $menuItems = $('.js-trigger'),
+        $item = $menuItems.find('li a'),
+        $category = $('.js-content').find('li'),
+        $currentCategory;
 
-	$item.on("click", function(e) {
-		e.preventDefault();
-		$menuItems.each(function() {
-			$item.removeClass("active");
-		});
-		var clickedItem = $(this);
-		clickedItem.toggleClass("active");
-		var currentCategory = clickedItem.text().replace(" ", "-");
-		$category.each(function() {
-			var categoryItem = $(this);
-			if (categoryItem.text().indexOf(currentCategory) <= 0) {
-				categoryItem.addClass("hidden");
-			}
-			else {
-				categoryItem.removeClass("hidden");
-			}
-		});
-		if (currentCategory == "All") {
-			$category.removeClass("hidden");
-		}
-	});
+    $item.on('click', function (e) {
+        e.preventDefault();
+        $menuItems.each(function () {
+            $item.removeClass('active');
+        });
+        var $clickedItem = $(this);
+        $clickedItem.toggleClass('active');
+        $currentCategory = $clickedItem.text().replace(' ', '-');
+        $category.each(function () {
+            var $categoryItem = $(this);
+            if ($categoryItem.text().indexOf($currentCategory) <= 0) {
+                $categoryItem.addClass('hidden');
+            }
+            $categoryItem.removeClass('hidden');
+        });
+        if ($currentCategory === 'All') {
+            $category.removeClass('hidden');
+        }
+    });
 });
